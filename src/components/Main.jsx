3467fab168ea71cc63bd4a06d25594a742d36b0b
natnael772 <natnaeldeyas0@gmail.com>
2023-11-07 09:42:06 +0300

    Main Component

---
+++ src/components/Main.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import ListBox from "./ListBox";
+import WatchedBox from "./WatchedBox";
+
+const average = (arr) =>
+  arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+
+function Main() {
+  return (
+    <main className="main">
+      <ListBox />
+      <WatchedBox />
+    </main>
+  );
+}
+export default Main;
