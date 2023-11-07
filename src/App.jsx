53b1687cb525133be6364ccfd009be6e82e6c949
natnael772 <natnaeldeyas0@gmail.com>
2023-11-07 09:37:32 +0300

    App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import NavBar from "./components/NavBar";
+import Main from "./components/Main";
 
-function App() {
-  return <></>;
+export default function App() {
+  return (
+    <>
+      <NavBar />
+      <Main />
+    </>
+  );
 }
-
-export default App;
