ca7243799e114351578b930e2fe4add9b798a35b
natnael772 <natnaeldeyas0@gmail.com>
2023-11-07 09:38:44 +0300

    Add NavBar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import Search from "./Search";
+import Logo from "./Logo";
+import NumResults from "./NumResults";
+
+function NavBar() {
+  return (
+    <nav className="nav-bar">
+      <Logo />
+      <Search />
+      <NumResults />
+    </nav>
+  );
+}
+
+export default NavBar;
